0b9c9ddccc815acdc66c72d006e26ba1569dea99
Kevin T. Coughlin <KevinTCoughlin@users.noreply.github.com>
2024-06-10 14:25:13 -0700

    Initial Commit

---
+++ tictactoe macOS/AppDelegate.swift
@@ -0,0 +1,29 @@
+//
+//  AppDelegate.swift
+//  tictactoe macOS
+//
+//  Created by Kevin T. Coughlin on 6/10/24.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationShouldTerminateAfterLastWindowClosed(_ sender: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
