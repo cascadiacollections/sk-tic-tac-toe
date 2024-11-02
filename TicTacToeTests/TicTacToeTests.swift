06a8f6fe90605386cf8cc58d0c4879d3b22f6a12
Kevin Coughlin <706967+KevinTCoughlin@users.noreply.github.com>
2024-11-01 22:08:08 -0700

    WIP - Add unit tests and encapsulate GameLogic

---
+++ TicTacToeTests/TicTacToeTests.swift
@@ -0,0 +1,16 @@
+//
+//  TicTacToeTests.swift
+//  TicTacToeTests
+//
+//  Created by Kevin T. Coughlin on 10/29/24.
+//
+
+import Testing
+
+struct TicTacToeTests {
+
+    @Test func testExample() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ tictactoe Shared/GameLogic.swift
@@ -0,0 +1,8 @@
+//
+//  GameLogic.swift
+//  tictactoe
+//
+//  Created by Kevin T. Coughlin on 10/29/24.
+//
+
+import Foundation
